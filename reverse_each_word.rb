d80f1147eb5209e2ea79dcf735a3bdbfed82c15c
Nathan Lewis <n.lewis.perc@gmail.com>
2020-11-25 02:28:17 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ require 'pry'
 
 def reverse_each_word(string)
   new_string = ""
-  string.split.each do |index|
+  string.split(" ").each do |index|
     new_string << index.reverse
     binding.pry
   end
